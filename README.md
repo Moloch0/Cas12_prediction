3137b34e51984cdea5f88d007f21575cf75713cd
Moloch0 <luoliheng@nwpu.mail.edu.cn>
2024-03-14 17:12:21 +0800

    modified:   .gitignore 	modified:   README.md 	new file:   prediction.py 	new file:   sub2/CasMINI/saved_best_model 	new file:   sub2/CasMINI/saved_best_model.pkl 	new file:   sub2/OsCas12f1/saved_best_model 	new file:   sub2/OsCas12f1/saved_best_model.pkl 	new file:   sub2/RhCas12f1/saved_best_model 	new file:   sub2/RhCas12f1/saved_best_model.pkl

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Cas12_prediction
+# Cas12f_prediction
+
+## 1. Introduction
+
+Cas12f_prediction is a Python script for predicting Cas12f. It takes a CSV file containing sequence information as input, performs prediction using a specified Cas12 (options include CasMINI, OsCas12f1, RhCas12f1), and outputs the prediction results to a CSV file.
+
+It requires `pandas` amd `joblib` two modules.
+
+```shell
+pip install pandas joblib
+```
+
+## 2. Argument Details
+
+`-i` or `--input_filename`: The name of the input file. It MUST be a two-column CSV file. This is a required argument.
+`-n` or `--Cas12f_name`: The name of the Cas12 to use for prediction. It must be one of ‘CasMINI’, ‘OsCas12f1’, ‘RhCas12f1’. This is a required argument.
+`-o` or `--output_filename`: The name of the output file. The default is ‘./output.csv’.
+
+## 3. Usage Example
+
+The input file should be a CSV file with two columns: ID and SEQ. SEQ should be a 24bp sequence, including a 4bp pam and a 20bp target. For example:
+
+```csv
+ID,SEQ
+seq1,TTTAACAGGGGATACACCTCCTCT
+```
+
+The command to run the script is as follows:
+
+```python
+python prediction.py -i input.csv -n CasMINI -o output.csv
+```
---
+++ prediction.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import joblib
+import argparse
+
+
+def get_parser():
+    desc = """
+    Program: Cas12_prediction
+    Version: 0.1
+    Author : Liheng Luo
+    Email  : <luoliheng@ibms.pumc.edu.cn>
+
+    Optional Cas12s: ['CasMINI', 'OsCas12f1', 'RhCas12f1']
+
+    Examples:
+        1) input file:
+                ID,SEQ(4bp pam + 20bp target)
+                seq1,TTTAACAGGGGATACACCTCCTCT
+
+           """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter, description=desc
+    )
+
+    parser.add_argument(
+        "-i",
+        "--input_filename",
+        type=str,
+        help="The name of the input file. MUST be a two-column .csv file",
+        required=True,
+    )
+    parser.add_argument(
+        "-n",
+        "--Cas12f_name",
+        choices=["CasMINI", "OsCas12f1", "RhCas12f1"],
+        type=str,
+        help="The name of the Cas12f to use for prediction.",
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--output_filename",
+        type=str,
+        default="./output.csv",
+        help="The name of the output file. (default='./output.csv')",
+    )
+
+    return parser
+
+
+def one_hot_encode(seq):
+
+    cols = [
+        f"{prefix}_{i}_{nuc}"
+        for prefix, end in [("PAM", 5), ("target", 21)]
+        for i in range(1, end)
+        for nuc in ["A", "T", "C", "G"]
+    ]
+    one_hot_df = pd.DataFrame(columns=cols, index=[0]).fillna(0)
+
+    for i, nucleotide in enumerate(seq):
+        col_name = f"pos_{i+1}_{nucleotide}"
+        one_hot_df.at[0, col_name] = 1
+
+    return one_hot_df
+
+
+def main(input_filename, model_name, output_filename):
+    input_df = pd.read_csv(input_filename)
+
+    model = joblib.load(f"./sub2/{model_name}/saved_best_model.pkl")
+
+    predictions = model.predict(
+        pd.concat(
+            [one_hot_encode(seq) for seq in input_df.iloc[:, 1]], ignore_index=True
+        )
+    )
+
+    input_df[f"{model_name}_efficiency"] = predictions
+
+    input_df.to_csv(output_filename, index=False)
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if args.Cas12f_name not in ["CasMINI", "OsCas12f1", "RhCas12f1"]:
+        parser.error("One of CasMINI, OsCas12f1, or RhCas12f1 must be chosen.")
+    main(args.input_filename, args.Cas12f_name, args.output_filename)
